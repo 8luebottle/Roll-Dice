7ea342268f45a931d3fb59d041ad636c1430000d
8luebottle <itbiz.irs@gmail.com>
2019-10-12 22:35:00 +0900

    Pseudo-Code

--- rolldice.py
+++ rolldice.py
@@ -1,1 +1,34 @@
+"""
+Python Sideproject by. 8luebottle
+[Roll-Dice]
+"""
+
+"""
+[Pseudo-Code]
+User Input : Would you like to play? 
+
+If Yes :
+    Roll dice for user
+    Roll dice for computer
+
+    If user wins:
+        'You Win!!'
+    elif dealer wins:
+        'You Lose'
+    else 
+        'Hive-five!'
+If No : 
+    'Bye~'
+
+Exception case: 
+    If user inputs other than Yes | No
+"""
 import random
+
+def main():
+    min = 1
+    max = 6
+
+    def roll_dice():
+
+main()
